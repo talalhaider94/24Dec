4830c1ece10445459f33d3f395314dc7bca831ed
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-06-12 10:21:03 +0200

    *translation to it lang*

--- Quantis.Workflow.Complete/ClientApp/src/app/views/loading-form/loading-form-user/loading-form-user.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/loading-form/loading-form-user/loading-form-user.component.ts
@@ -30,6 +30,28 @@ export class LoadingFormUserComponent implements OnInit, OnDestroy {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10,
+      language: {
+        processing: "Elaborazione...",
+        search: "Cerca:",
+        lengthMenu: "Visualizza _MENU_ elementi",
+        info: "Vista da _START_ a _END_ di _TOTAL_ elementi",
+        infoEmpty: "Vista da 0 a 0 di 0 elementi",
+        infoFiltered: "(filtrati da _MAX_ elementi totali)",
+        infoPostFix: "",
+        loadingRecords: "Caricamento...",
+        zeroRecords: "La ricerca non ha portato alcun risultato.",
+        emptyTable: "Nessun dato presente nella tabella.",
+        paginate: {
+          first: "Primo",
+          previous: "Precedente",
+          next: "Seguente",
+          last: "Ultimo"
+        },
+        aria: {
+          sortAscending: ": attiva per ordinare la colonna in ordine crescente",
+          sortDescending: ":attiva per ordinare la colonna in ordine decrescente"
+        }
+      }
     };
     const currentUser = this.authService.getUser();
     this.loadingFormService.getFormsByUserId(currentUser.userid).pipe(first()).subscribe(data => {
