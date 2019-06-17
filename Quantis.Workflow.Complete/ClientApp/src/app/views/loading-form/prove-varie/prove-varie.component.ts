66ee6402834a45d3a77a93753c8171f02bccbbfa
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-06-17 11:08:59 +0200

    fix

--- Quantis.Workflow.Complete/ClientApp/src/app/views/loading-form/prove-varie/prove-varie.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/loading-form/prove-varie/prove-varie.component.ts
@@ -115,7 +115,7 @@ export class ProveVarieComponent implements OnInit {
   ngOnInit() {
     const currentUser = this.authService.getUser();
     this.isAdmin = currentUser.isadmin;
-    this.dtOptions2 = {
+    /*this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10,
       language: {
@@ -140,7 +140,7 @@ export class ProveVarieComponent implements OnInit {
           sortDescending: ":attiva per ordinare la colonna in ordine decrescente"
         }
       }
-    };
+    };*/
     this.activatedRoute.paramMap.subscribe(params => {
       this.formId = params.get("formId");
       this.formName = params.get("formName");
