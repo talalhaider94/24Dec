9394bfc52bc5c23cdf861c45e175ae4b11c3f80d
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-06-18 11:29:33 +0200

    Temporary checkSession to logout after 15min of no actions

--- Quantis.Workflow.Complete/ClientApp/src/app/app.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'body',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+
+  ) { }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
       }
+      this.authService.checkSession();
       window.scrollTo(0, 0);
     });
   }
