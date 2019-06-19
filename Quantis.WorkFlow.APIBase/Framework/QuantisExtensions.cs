1ba0c5594fe4879b556246efdd8379367f87cd2c
Shahzad Amin <shahzad.744@gmail.com>
2019-06-19 16:03:35 +0200

    API level Seach,Sort,pagging done

--- Quantis.WorkFlow.APIBase/Framework/QuantisExtensions.cs
+++ Quantis.WorkFlow.APIBase/Framework/QuantisExtensions.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Quantis.WorkFlow.Services.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+using System.Linq.Dynamic.Core;
 namespace Quantis.WorkFlow.APIBase.Framework
 {
-    class QuantisExtensions
+    public static class QuantisExtensions
     {
+        public static PagedList<T> GetPaged<T>(this IQueryable<T> query,
+                                         PagingInfo pagingInfo) where T : class
+        {
+            var CurrentPage = pagingInfo.Index;
+            var PageSize = pagingInfo.Size;
+            var RowCount = query.Count();
+            var skip = (pagingInfo.Index - 1) * pagingInfo.Size;
+            if (pagingInfo.OrderDirection == OrderDirection.Desc)
+            {
+                var results = query.OrderBy(pagingInfo.OrderBy+" DESC").Skip(skip).Take(pagingInfo.Size).ToList();
+                return new PagedList<T>(results, CurrentPage, PageSize, RowCount);
+            }
+            else
+            {
+                var results = query.OrderBy(pagingInfo.OrderBy).Skip(skip).Take(pagingInfo.Size).ToList();
+                return new PagedList<T>(results, CurrentPage, PageSize, RowCount);
+            }
+            
+        }
+
+
     }
 }
