83bb00c80557d0eea6bb9c24588db1cb5c01ad2a
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-06-21 12:48:32 +0200

    archived kpi by carmine

--- Quantis.Workflow.Complete/ClientApp/src/app/views/archivedkpi/archivedkpi.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/archivedkpi/archivedkpi.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Pipe } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { ApiService } from '../../_services/api.service';
-import { Subject } from 'rxjs';
+import { Subject, from, BehaviorSubject, interval } from 'rxjs';
+import { DatePipe, formatDate,getLocaleDateFormat } from '@angular/common'
+import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
+import * as moment from 'moment';
+import { first } from 'rxjs/operators';
+import { Key } from 'protractor';
+
+
 
 declare var $;
 var $this;
 
+var nome='';
 
 @Component({
   templateUrl: './archivedkpi.component.html'
@@ -15,6 +23,9 @@ export class ArchivedKpiComponent implements OnInit {
   @ViewChild('searchCol1') searchCol1: ElementRef;
   @ViewChild(DataTableDirective) private datatableElement: DataTableDirective;
 
+  contratti=[];
+
+ 
   dtOptions: DataTables.Settings = {
     language: {
       processing: "Elaborazione...",
@@ -43,7 +54,7 @@ export class ArchivedKpiComponent implements OnInit {
   modalData = {
     id_kpi: '',
     name_kpi: '',
-    interval_kpi: '',
+    interval_kpi: null,
     value_kpi: '',
     ticket_id: '',
     close_timestamp_ticket: '',
@@ -51,8 +62,32 @@ export class ArchivedKpiComponent implements OnInit {
   };
 
   kpisData = [];
+datiGrezzi=[];
+countCampiData=[];
+  p = null;
+
+  fitroDataById: any = [
+    {
+      event_type_id: ' event_type_id  ',
+      resource_id: 'resource_id',
+      time_stamp : 'time_stamp ',
+      raw_data_id: 'raw_data_id',
+      create_date : 'create_date ',
+      data:this.datiGrezzi,
+      modify_date:'modify_date',
+      reader_id: 'reader_id',
+      event_source_type_id : 'event_source_type_id ',
+      event_state_id: 'event_state_id ',
+      partner_raw_data_id : 'partner_raw_data_id ',
+    }
+  ]
+  
+
 
   dtTrigger: Subject<any> = new Subject();
+
+
+
   ArchivedKpiBodyData: any = [
     {
       id_kpi: 'id_kpi',
@@ -64,14 +99,32 @@ export class ArchivedKpiComponent implements OnInit {
       archived: 'archived'
     }
   ]
+
+
+
+  
   monthVar: any;
   yearVar: any;
+  id:any;
+  
   constructor(private apiService: ApiService) {
     $this = this;
+     
+   
+
   }
 
   ngOnInit() {
+  // this.getdati('2221','03','2019');
+   //  this.getdati('2221','03','2019');
+   this.monthVar = moment().subtract(1, 'month').format('MM');
+   this.yearVar = moment().subtract(1, 'month').format('YYYY');
+   //this.spit();
+  
   }
+
+  //moment('01/'+dataIngresso).format('MM');
+ //moment('01/'+dataIngresso).format('YYYY');
 
   populateModalData(data) {
     // this.modalData.id_kpi = data.id_kpi;
@@ -82,15 +135,22 @@ export class ArchivedKpiComponent implements OnInit {
     // this.modalData.close_timestamp_ticket = data.close_timestamp_ticket;
     // this.modalData.archived = data.archived;
     this.apiService.getAllArchivedKpis(data.id_kpi).subscribe((kpis: any) => {
-      this.kpisData = kpis;
+    this.kpisData = kpis;
+    console.log('pop',this.kpisData);
+    
+
+      
     });
 
   }
+
+
 
   populateDateFilter() {
     this.apiService.getDateKpis(this.monthVar, this.yearVar).subscribe((data: any) => {
       this.ArchivedKpiBodyData = data;
       this.rerender();
+      
     });
 
   }
@@ -151,8 +211,15 @@ export class ArchivedKpiComponent implements OnInit {
 
   getKpis() {
     this.apiService.getArchivedKpis().subscribe((data) =>{
+      //this.date=new Date();
+      //let latest_date =this.datepipe.transform(this.date, 'yyyy-MM-dd');
+      
+    
       this.ArchivedKpiBodyData = data;
+      Object.keys(this.fitroDataById).forEach( key => {
+        this.fitroDataById[key].data = JSON.parse(this.fitroDataById[key].data);
       console.log('Archived Kpis ', data);
+      })
     });
   }
 
@@ -161,4 +228,86 @@ export class ArchivedKpiComponent implements OnInit {
     });
   }
 
+  /*get key(){
+    return Object.keys(this.contratti);
+  }*/
+  
+getdati(id_kpi, month = this.monthVar, year = this.yearVar){
+  
+  
+  this.apiService.getDateKpisById(id_kpi,month, year).subscribe((dati: any) =>{
+  
+  this.fitroDataById = dati;
+  Object.keys(this.fitroDataById).forEach( key => {
+    this.fitroDataById[key].data = JSON.parse(this.fitroDataById[key].data);
+    
+  })
+  /*this.fitroDataById.forEach( d => {
+    let new_d = JSON.parse(d.data);
+    console.log(this.fitroDataById[0])
+    //this.fitroDataById.[d].data = new_d;
+  })*/
+  console.log('dati',dati);
+  this.getCountCampiData();
+  });
+
+
+
+}
+
+
+/*spit(){
+  const [first, second] = "10AM-4PM".split('-');
+  console.log(first);
+  console.log(second);
+}*/
+
+getDatiSecondPop(id_kpi, interval){
+
+      var mese=moment(interval).format('MM');
+      var anno=moment(interval).format('YYYY');
+  this.getdati(id_kpi,mese,anno);
+
+
+
+        
+      /*let resources = data["ArchivedKpiBodyData"];
+      let resource = resources["interval_kpi"];
+      console.log('stampa',resource);*/
+
+  
+
+  
+
+
+
+}
+
+
+getCountCampiData(){
+  let maxLength = 0;
+  this.fitroDataById.forEach( f => {
+    //let data = JSON.parse(f.data);
+    if(Object.keys(f.data).length > maxLength){
+      maxLength = Object.keys(f.data).length;
+    }  
+  });
+  this.countCampiData = [];
+  for(let i=1;i<= maxLength; i++){
+    this.countCampiData.push(i);
+  }
+}
+
+
+/*getdati(id_kpi,monthVar,yearVar){
+  this.apiService.getDateKpisById(id_kpi,monthVar,yearVar).subscribe(data => {
+    let res = data[0]["fitroDataById"]; 
+    let datiGrezzi = res['data'];
+    this.datiGrezzi=.datiGrezzi 
+    // this.dataChange.next(data) 
+     console.log(this.datiGrezzi);
+    });
+}*/
+
+  
 }
