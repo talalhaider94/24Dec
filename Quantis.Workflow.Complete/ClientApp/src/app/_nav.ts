130fa1ee2eeca642e1bf33c559cc6ee366f3d09e
Syed Danial Munsif <internetdreamz@gmail.com>
2019-07-16 02:11:56 +0500

    version updated

--- Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
@@ -77,14 +77,14 @@ export const navItems: NavData[] = [
         name: 'KPI in Verifica',
         url: '/workflow/verifica',
         icon: 'fa fa-file-text-o',
-        version: '0.0.5',
+        version: '0.0.6',
         key: 'VIEW_WORKFLOW_KPI_VERIFICA'
       },
       {
         name: 'Ricerca',
         url: '/workflow/ricerca',
         icon: 'fa fa-search',
-        version: '0.0.3',
+        version: '0.0.4',
         key: ['VIEW_WORKFLOW_RICERCA']
       },
     ]
