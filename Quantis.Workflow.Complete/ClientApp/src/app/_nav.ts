f5034218d75b9766708613cfd506ff4465bfcc01
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-07-17 10:05:45 +0200

    version

--- Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
@@ -163,7 +163,7 @@ export const navItems: NavData[] = [
         name: 'Utente',
         url: '/loading-form/utente',
         icon: 'fa fa-user-circle-o',
-        version: '0.0.5',
+        version: '0.0.7',
         key: 'VIEW_LOADING_FORM_UTENTI'
       },
     ]
