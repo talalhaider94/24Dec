e27b0fd3117bd191e7854a8b70a73b15a984d56c
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-07-19 12:29:43 +0200

    fix

--- Quantis.Workflow.Complete/ClientApp/src/app/_helpers/error-interceptor.service.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/_helpers/error-interceptor.service.ts
@@ -27,6 +27,9 @@ export class ErrorInterceptorService implements HttpInterceptor {
         this.router.navigate(['/login']);
         this.toastr.error('Sessione Scaduta', 'Effettuare nuovamente il login');
       }
+      if (err.status >= 500 && err.status < 600) {
+        this.toastr.error('Errore durante la comunicazione con il server');
+      }
       return throwError(err);
     }))
   }
