d391b354b1fc1c9e3e1f5a639484e97317197e41
Shahzad Amin <shahzad.744@gmail.com>
2019-08-06 20:37:01 +0500

    API for cost trend..

--- Quantis.WorkFlow.APIBase/API/WidgetService.cs
+++ Quantis.WorkFlow.APIBase/API/WidgetService.cs
@@ -1,22 +1,106 @@
-﻿using Quantis.WorkFlow.Services.API;
+﻿using Quantis.WorkFlow.APIBase.Framework;
+using Quantis.WorkFlow.Services.API;
 using Quantis.WorkFlow.Services.DTOs.Widgets;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Quantis.WorkFlow.APIBase.API
 {
     public class WidgetService : IWidgetService
     {
+        private readonly WorkFlowPostgreSqlContext _dbcontext;
+        public WidgetService(WorkFlowPostgreSqlContext dbcontext){
+            _dbcontext = dbcontext;
+        }
         public List<XYDTO> GetKPICountTrend(WidgetwithAggOptionDTO dto)
         {
-            var list = new List<XYDTO>();
-            list.Add(new XYDTO() { XValue = "03/19", YValue = 10 });
-            list.Add(new XYDTO() { XValue = "04/19", YValue = 11 });
-            list.Add(new XYDTO() { XValue = "05/19", YValue = 12 });
-            list.Add(new XYDTO() { XValue = "06/19", YValue = 11 });
-            list.Add(new XYDTO() { XValue = "07/19", YValue = 14 });
-            return list;
+            var result = new List<XYDTO>();
+            var facts= _dbcontext.SDMTicketFact.Where(o => o.period_month >= dto.DateRange.Item1.Month && o.period_year >= dto.DateRange.Item1.Year && o.period_month <= dto.DateRange.Item2.Month && o.period_year <= dto.DateRange.Item2.Year);
+            if (dto.KPIs.Any())
+            {
+                facts = facts.Where(o => dto.KPIs.Contains(o.global_rule_id));
+            }
+            if (dto.Measures.FirstOrDefault() == Measures.Number_of_ticket_in_KPI_in_Verifica)
+            {
+                if (dto.AggregationOption == AggregationOption.ANNAUL.Key)
+                {
+                    result = facts.GroupBy(o => o.period_year).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key + "",
+                        YValue = p.Count()
+                    }).OrderBy(o=>o.XValue).ToList();
+                }
+                else
+                {
+                    result = facts.GroupBy(o => new { o.period_year,o.period_month }).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key.period_month + "/"+p.Key.period_year,
+                        YValue = p.Count()
+                    }).ToList();
+                }
+            }
+            else if (dto.Measures.FirstOrDefault() == Measures.Number_of_ticket_of_KPI_Compliant)
+            {
+                if (dto.AggregationOption == AggregationOption.ANNAUL.Key)
+                {
+                    result = facts.GroupBy(o => o.period_year).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key + "",
+                        YValue = p.Count(r=>r.complaint)
+                    }).OrderBy(o => o.XValue).ToList();
+                }
+                else
+                {
+                    result = facts.GroupBy(o => new { o.period_year, o.period_month }).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key.period_month + "/" + p.Key.period_year,
+                        YValue = p.Count(r => r.complaint)
+                    }).ToList();
+                }
+            }
+            else if (dto.Measures.FirstOrDefault() == Measures.Number_of_ticket_of_KPI_Non_Calcolato)
+            {
+                if (dto.AggregationOption == AggregationOption.ANNAUL.Key)
+                {
+                    result = facts.GroupBy(o => o.period_year).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key + "",
+                        YValue = p.Count(r => r.notcalculated)
+                    }).OrderBy(o => o.XValue).ToList();
+                }
+                else
+                {
+                    result = facts.GroupBy(o => new { o.period_year, o.period_month }).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key.period_month + "/" + p.Key.period_year,
+                        YValue = p.Count(r => r.notcalculated)
+                    }).ToList();
+                }
+            }
+            else if (dto.Measures.FirstOrDefault() == Measures.Number_of_ticket_of_KPI_Non_Compliant)
+            {
+                if (dto.AggregationOption == AggregationOption.ANNAUL.Key)
+                {
+                    result = facts.GroupBy(o => o.period_year).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key + "",
+                        YValue = p.Count(r => r.notcomplaint)
+                    }).OrderBy(o => o.XValue).ToList();
+                }
+                else
+                {
+                    result = facts.GroupBy(o => new { o.period_year, o.period_month }).Select(p => new XYDTO()
+                    {
+                        XValue = p.Key.period_month + "/" + p.Key.period_year,
+                        YValue = p.Count(r => r.notcomplaint)
+                    }).ToList();
+                }
+            }
+            return result;
+
+
         }
     }
 }
