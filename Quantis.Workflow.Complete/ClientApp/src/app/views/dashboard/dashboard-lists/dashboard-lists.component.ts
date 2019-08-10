6820197d1f63f61274f43006519ce3da2b675c96
Syed Danial Munsif <internetdreamz@gmail.com>
2019-08-10 15:57:51 +0500

    dashboard list view added and other ui changes

---
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/dashboard/dashboard-lists/dashboard-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-lists',
+  templateUrl: './dashboard-lists.component.html',
+  styleUrls: ['./dashboard-lists.component.scss']
+})
+export class DashboardListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
