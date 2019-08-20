51cd3ea4c9e4521129eea80c96d46ade5b37b02e
Syed Danial Munsif <internetdreamz@gmail.com>
2019-08-20 23:36:55 +0500

    Edit Widget name done

--- Quantis.Workflow.Complete/ClientApp/src/app/widgets/barchart/barchart.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/widgets/barchart/barchart.component.ts
@@ -23,18 +23,22 @@ export class BarchartComponent implements OnInit {
 
 	loading: boolean = true;
 	barChartWidgetParameters: any;
+	editWidgetName: boolean = true;
 	@Output()
 	barChartParent = new EventEmitter<any>();
 	constructor(
 		private dashboardService: DashboardService,
 		private emitter: EmitterService,
 		private dateTime: DateTimeService,
-		private router:Router
+		private router: Router
 	) { }
 
 	ngOnInit() {
-		console.log('CURRENT ROUTE', this.router.url)
-		console.log('BarchartComponent', this.widgetname, this.url, this.id, this.widgetid ,this.filters, this.properties);
+		console.log('CURRENT ROUTE', this.router.url);
+		if (this.router.url.includes('dashboard/public')) {
+			this.editWidgetName = false;
+		}
+		console.log('BarchartComponent', this.widgetname, this.url, this.id, this.widgetid, this.filters, this.properties);
 		if (this.url) {
 			this.emitter.loadingStatus(true);
 			// this.getWidgetParameters(this.url);
@@ -45,7 +49,6 @@ export class BarchartComponent implements OnInit {
 		this.emitter.getData().subscribe(result => {
 			const { type, data } = result;
 			if (type === 'barChart') {
-				debugger;
 				let currentWidgetId = result.data.barChartWidgetParameters.id;
 				if (currentWidgetId === this.id) {
 					setTimeout(() => {
@@ -143,7 +146,7 @@ export class BarchartComponent implements OnInit {
 
 	openModal() {
 		console.log('OPEN MODAL BAR CHART', JSON.stringify(this.barChartWidgetParameters));
-		this.barChartParent.emit({ 
+		this.barChartParent.emit({
 			type: 'openBarChartModal',
 			data: {
 				barChartWidgetParameters: this.barChartWidgetParameters,
@@ -159,7 +162,7 @@ export class BarchartComponent implements OnInit {
 					},
 				}
 			}
-		 });
+		});
 	}
 	closeModal() {
 		this.barChartParent.emit({ type: 'closeModal' });
@@ -182,7 +185,7 @@ export class BarchartComponent implements OnInit {
 			let measureIndex = dashboardComponentData.barChartWidgetParameterValues.Properties.measure;
 			label = dashboardComponentData.barChartWidgetParameters.measures[measureIndex];
 		}
-		if(currentWidgetComponentData) {
+		if (currentWidgetComponentData) {
 			// setting chart label and type on first load
 			label = currentWidgetComponentData.measures[0];
 			this.barChartType = Object.keys(currentWidgetComponentData.charttypes)[0];
@@ -195,9 +198,9 @@ export class BarchartComponent implements OnInit {
 		this.closeModal();
 	}
 
-	widgetnameChange(event){
+	widgetnameChange(event) {
 		console.log('WIDGET NAME CHANE', event)
-		this.barChartParent.emit({ 
+		this.barChartParent.emit({
 			type: 'changeBarChartWidgetName',
 			data: {
 				barChart: {
@@ -206,6 +209,6 @@ export class BarchartComponent implements OnInit {
 					widgetid: this.widgetid
 				}
 			}
-		 });
+		});
 	}
 }
