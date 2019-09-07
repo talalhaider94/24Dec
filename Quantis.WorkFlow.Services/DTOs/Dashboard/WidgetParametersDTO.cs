36a8f0ece1f8ca19097fca01b7e37554f9b7d9f8
Shahzad Amin <shahzad.744@gmail.com>
2019-09-07 19:50:06 +0500

    Customer Id Fixes

--- Quantis.WorkFlow.Services/DTOs/Dashboard/WidgetParametersDTO.cs
+++ Quantis.WorkFlow.Services/DTOs/Dashboard/WidgetParametersDTO.cs
@@ -7,6 +7,7 @@ namespace Quantis.WorkFlow.Services.DTOs.Dashboard
     public class WidgetParametersDTO
     {
         public int GlobalFilterId { get; set; }
+        public int UserId { get; set; }
         public Dictionary<string,string> Properties { get; set; }
         public Dictionary<string, string> Filters { get; set; }
     }
