bd8f7ebdc20d48bb8fef8e18158044785a936ed1
Syed Danial Munsif <internetdreamz@gmail.com>
2019-09-12 13:07:52 +0500

    model updated

--- Quantis.Workflow.Complete/ClientApp/src/app/widgets/kpi-status-summary/kpi-status-summary.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/widgets/kpi-status-summary/kpi-status-summary.component.ts
@@ -9,144 +9,141 @@ import { Router } from '@angular/router';
   styleUrls: ['./kpi-status-summary.component.scss']
 })
 export class KpiStatusSummaryComponent implements OnInit {
-	@Input() widgetname: string;
-	@Input() url: string;
-	@Input() filters: Array<any>;
-	@Input() properties: Array<any>;
-	@Input() widgetid: number;
-	@Input() dashboardid: number;
-	@Input() id: number;
-	loading: boolean = true;
-	kpiStatusSummaryWidgetParameters: any;
-	setWidgetFormValues: any;
-	editWidgetName: boolean = true;
-	@Output()
+  @Input() widgetname: string;
+  @Input() url: string;
+  @Input() filters: Array<any>;
+  @Input() properties: Array<any>;
+  @Input() widgetid: number;
+  @Input() dashboardid: number;
+  @Input() id: number;
+  loading: boolean = true;
+  kpiStatusSummaryWidgetParameters: any;
+  setWidgetFormValues: any;
+  editWidgetName: boolean = true;
+  @Output()
   kpiStatusSummaryParent = new EventEmitter<any>();
   kpiStatusSummaryData: any = [];
 
-	constructor(
-		private dashboardService: DashboardService,
-		private emitter: EmitterService,
-		private dateTime: DateTimeService,
-		private router: Router
-	) { }
+  constructor(
+    private dashboardService: DashboardService,
+    private emitter: EmitterService,
+    private dateTime: DateTimeService,
+    private router: Router
+  ) { }
 
-	ngOnInit() {
-		console.log('KpiStatusSummary Table', this.widgetname, this.url, this.id, this.widgetid, this.filters, this.properties);
-		if (this.router.url.includes('dashboard/public')) {
-			this.editWidgetName = false;
-		}
-		if (this.url) {
-			this.emitter.loadingStatus(true);
-			this.getChartParametersAndData(this.url);
-		}
-		// coming from dashboard or public parent components
-		this.subscriptionForDataChangesFromParent()
-	}
+  ngOnInit() {
+    console.log('KpiStatusSummary Table', this.widgetname, this.url, this.id, this.widgetid, this.filters, this.properties);
+    if (this.router.url.includes('dashboard/public')) {
+      this.editWidgetName = false;
+    }
+    if (this.url) {
+      this.emitter.loadingStatus(true);
+      this.getChartParametersAndData(this.url);
+    }
+    // coming from dashboard or public parent components
+    this.subscriptionForDataChangesFromParent()
+  }
 
-	subscriptionForDataChangesFromParent() {
-		this.emitter.getData().subscribe(result => {
-			const { type, data } = result;
-			if (type === 'kpiStatusSummaryTable') {
-				let currentWidgetId = data.kpiStatusSummaryWidgetParameters.id;
-				if (currentWidgetId === this.id) {
-					// updating parameter form widget setValues 
-					let kpiStatusSummaryTableFormValues = data.barChartWidgetParameterValues;
-					kpiStatusSummaryTableFormValues.Filters.daterange = this.dateTime.buildRangeDate(kpiStatusSummaryTableFormValues.Filters.daterange);
-					this.setWidgetFormValues = kpiStatusSummaryTableFormValues;
-					this.updateChart(data.result.body, data, null);
-				}
-			}
-		});
-	}
-	// invokes on component initialization
-	getChartParametersAndData(url) {
-		let myWidgetParameters = null;
-		this.dashboardService.getWidgetParameters(url).pipe(
-			mergeMap((getWidgetParameters: any) => {
+  subscriptionForDataChangesFromParent() {
+    this.emitter.getData().subscribe(result => {
+      const { type, data } = result;
+      if (type === 'kpiStatusSummaryTable') {
+        let currentWidgetId = data.kpiStatusSummaryWidgetParameters.id;
+        if (currentWidgetId === this.id) {
+          // updating parameter form widget setValues 
+          let kpiStatusSummaryTableFormValues = data.kpiStatusSummaryWidgetParameterValues;
+          kpiStatusSummaryTableFormValues.Filters.daterange = this.dateTime.buildRangeDate(kpiStatusSummaryTableFormValues.Filters.daterange);
+          this.setWidgetFormValues = kpiStatusSummaryTableFormValues;
+          this.updateChart(data.result.body, data, null);
+        }
+      }
+    });
+  }
+  // invokes on component initialization
+  getChartParametersAndData(url) {
+    let myWidgetParameters = null;
+    this.dashboardService.getWidgetParameters(url).pipe(
+      mergeMap((getWidgetParameters: any) => {
         myWidgetParameters = getWidgetParameters;
-				// Map Params for widget index when widgets initializes for first time
-				let newParams = WidgetsHelper.initWidgetParameters(getWidgetParameters, this.filters, this.properties);
-				return this.dashboardService.getWidgetIndex(url, newParams);
-			})
-		).subscribe(getWidgetIndex => {
+        // Map Params for widget index when widgets initializes for first time
+        let newParams = WidgetsHelper.initWidgetParameters(getWidgetParameters, this.filters, this.properties);
+        return this.dashboardService.getWidgetIndex(url, newParams);
+      })
+    ).subscribe(getWidgetIndex => {
       // populate modal with widget parameters
-			let kpiStatusSummaryParams;
-			if (myWidgetParameters) {
-				kpiStatusSummaryParams = {
-					type: 'kpiStatusSummaryTableParams',
-					data: {
-						...myWidgetParameters,
-						widgetname: this.widgetname,
-						url: this.url,
-						filters: this.filters,
-						properties: this.properties,
-						widgetid: this.widgetid,
-						dashboardid: this.dashboardid,
-						id: this.id
-					}
-				}
-				this.kpiStatusSummaryWidgetParameters = kpiStatusSummaryParams.data;
-				// setting initial Paramter form widget values
-				this.setWidgetFormValues = WidgetsHelper.initWidgetParameters(myWidgetParameters, this.filters, this.properties);
-				console.log('KpiStatusSummary Table this.setWidgetFormValues', this.setWidgetFormValues);
-			}
-			// popular chart data
-			if (getWidgetIndex) {
-				const chartIndexData = getWidgetIndex.body;
-				// third params is current widgets settings current only used when
-				// widgets loads first time. may update later for more use cases
-				this.updateChart(chartIndexData, null, kpiStatusSummaryParams.data);
-			}
-			this.loading = false;
-			this.emitter.loadingStatus(false);
-		}, error => {
-			console.error('KpiStatusSummary Table', error, this.setWidgetFormValues);
-			this.loading = false;
-			this.emitter.loadingStatus(false);
-		});
-	}
+      let kpiStatusSummaryParams;
+      if (myWidgetParameters) {
+        kpiStatusSummaryParams = {
+          type: 'kpiStatusSummaryTableParams',
+          data: {
+            ...myWidgetParameters,
+            widgetname: this.widgetname,
+            url: this.url,
+            filters: this.filters,
+            properties: this.properties,
+            widgetid: this.widgetid,
+            dashboardid: this.dashboardid,
+            id: this.id
+          }
+        }
+        this.kpiStatusSummaryWidgetParameters = kpiStatusSummaryParams.data;
+        // setting initial Paramter form widget values
+        this.setWidgetFormValues = WidgetsHelper.initWidgetParameters(myWidgetParameters, this.filters, this.properties);
+        console.log('KpiStatusSummary Table this.setWidgetFormValues', this.setWidgetFormValues);
+      }
+      // popular chart data
+      if (getWidgetIndex) {
+        const chartIndexData = getWidgetIndex.body;
+        // third params is current widgets settings current only used when
+        // widgets loads first time. may update later for more use cases
+        this.updateChart(chartIndexData, null, kpiStatusSummaryParams.data);
+      }
+      this.loading = false;
+      this.emitter.loadingStatus(false);
+    }, error => {
+      console.error('KpiStatusSummary Table', error, this.setWidgetFormValues);
+      this.loading = false;
+      this.emitter.loadingStatus(false);
+    });
+  }
 
 
-	openModal() {
-		this.kpiStatusSummaryParent.emit({
-			type: 'openKpiStatusSummaryModal',
-			data: {
-				kpiStatusSummaryWidgetParameters: this.kpiStatusSummaryWidgetParameters,
-				setWidgetFormValues: this.setWidgetFormValues,
-				isBarChartComponent: true
-			}
-		});
+  openModal() {
+    this.kpiStatusSummaryParent.emit({
+      type: 'openKpiStatusSummaryModal',
+      data: {
+        kpiStatusSummaryWidgetParameters: this.kpiStatusSummaryWidgetParameters,
+        setWidgetFormValues: this.setWidgetFormValues,
+        isKpiStatusSummaryComponent: true
+      }
+    });
   }
-  
-	closeModal() {
-		this.emitter.sendNext({ type: 'closeModal' });
+
+  closeModal() {
+    this.emitter.sendNext({ type: 'closeModal' });
   }
-  
-	// dashboardComponentData is result of data coming from 
-	updateChart(chartIndexData, dashboardComponentData, currentWidgetComponentData) {
-    debugger
-    // setTimeout(() => {
-      this.kpiStatusSummaryData = chartIndexData;
-    // });
-    
+
+  // dashboardComponentData is result of data coming from 
+  updateChart(chartIndexData, dashboardComponentData, currentWidgetComponentData) {
+    this.kpiStatusSummaryData = chartIndexData;
+
     if (dashboardComponentData) {
     }
-    
-		if (currentWidgetComponentData) {
-		}
 
-	}
+    if (currentWidgetComponentData) {
+    }
 
-	widgetnameChange(event) {
-		console.log('widgetnameChange', this.id, event);
-		this.emitter.sendNext({
-			type: 'changeWidgetName',
-			data: {
-				widgetname: event,
-				id: this.id,
-				widgetid: this.widgetid
-			}
-		});
-	}
+  }
+
+  widgetnameChange(event) {
+    console.log('widgetnameChange', this.id, event);
+    this.emitter.sendNext({
+      type: 'changeWidgetName',
+      data: {
+        widgetname: event,
+        id: this.id,
+        widgetid: this.widgetid
+      }
+    });
+  }
 }
