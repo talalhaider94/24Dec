145a8c0df2a256b6965620d33c949f5da41e3e68
Giacomo Visentin <giacomo.visentin@quantis.it>
2019-11-12 09:01:29 +0100

    fix

--- Quantis.Workflow.Complete/ClientApp/src/app/views/workfloworganizationunit/userprofiling.module.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/workfloworganizationunit/userprofiling.module.ts
@@ -7,13 +7,14 @@ import { DataTablesModule } from 'angular-datatables';
 import { AngularDualListBoxModule } from 'angular-dual-listbox';
 import { FormsModule } from '@angular/forms';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { FilterUsersPipe } from './../../_pipes/filterUsersWF.pipe';
 //import { TreeViewComponent } from '@syncfusion/ej2-angular-navigations';
 import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @NgModule({
-    declarations: [UserProfilingComponent],
+  declarations: [UserProfilingComponent, FilterUsersPipe],
     imports: [
         //AppModule,
         CommonModule,
