75408cf9fc721fb3322801b0288ca81d4670d72d
Talal Haider <talalhaider93@gmail.com>
2019-11-15 13:19:13 +0500

    Standard Dashboard update

--- Quantis.Workflow.Complete/ClientApp/src/app/views/dashboard/landing-page-details/landing-page-details.component.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/dashboard/landing-page-details/landing-page-details.component.ts
@@ -367,10 +367,11 @@ export class LandingPageDetailsComponent implements OnInit {
     }
 
     showNonCompliantModal(contract) {
+        this.selectedContract = contract;
         var kpi= this.orignalArray
         var filteredkpi = kpi.filter(a => a.contractname == contract)
         if (filteredkpi[0].noncomplaintkpis != 0){
-            this.KpiNonCompliants = filteredkpi[0].bestkpis;
+            this.KpiNonCompliants = filteredkpi[0].worstkpis;
             console.log(this.KpiNonCompliants,'non kpiCompiant')
         }
         this.rerender();
